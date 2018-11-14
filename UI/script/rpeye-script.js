6865f2b081c29ca9006cd765e444c6523e8158e9
Judy Z <2j.zy.zhang@gmail.com>
2018-11-13 20:15:27 -0500

    added filter add button
    Add button still doesn't work

--- UI/script/rpeye-script.js
+++ UI/script/rpeye-script.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
+	//input cursor start with search bar
 	$("input").focus();
-	
-	/*filter block renering*/
-	var filter = "<form id=\"filter\"></form>";
-	var result = ""
-	var content = null;
-	var result_num = 10;
-    $("#filter_btn").click(function(){
-  		if ( filter ) {
-  			/*swap content*/
-  			$("#filter_btn").text("Save");
-    		content=$("#content").children().detach();
-    		$("#content").prepend($(filter));
 
-    		filter = null;
+	/*filter block rendering*/
+	//var num_res = "<div class=\"input-group\"><input id=\"min\" class=\"btn\" type=\"button\" value=\"-\"><span id=\"num_res_value\">7</span><input id=\"add\" class=\"btn\" type=\"button\" value=\"+\"></div>";
+	var num_res="<input id=\"clicker\" class=\"btn\" type=\"button\" value=\"+\">";
+	var filter = "<div id=\"filter\">"+num_res+"</div>";
+	var content = null;
+	/*based swap filter and content*/
+    $("#filter_btn").click(function(){
+  		//show filter content
+  		if ( filter ) {
+  			$("#filter_btn").text("Save"); //change filter button text
+    		content=$("#content").children().detach(); //swap out content
+    		$("#content").prepend($(filter)); //show filter content
+    		filter = null; //clear out filter holder
+    	//hide filter content, show back result page
   		} else {
   			/*might need to change this*/
   			$("#filter_btn").text("Filter");
-
     		filter = $( "#filter" ).detach();
     		/*show original content*/
     		$("#content").prepend($(content));
   		}
     });
+    console.log("ready");
+
+	/*In filter, change number of result displayed in the page*/
+   /*	$( "#clicker" ).on( "click", function() {
+   		alert("clicked");
+	});*/
 });
