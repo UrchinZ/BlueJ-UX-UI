91d20f854414714e026861a08243331cc21c6344
Jared <segalj@rpi.edu>
2018-12-10 11:39:36 -0500

    moved the UI updater to APIManager to be able to use it synchronusly

--- UI/javascript/APIManager.js
+++ UI/javascript/APIManager.js
@@ -1,9 +1,10 @@
+
+//class to deal with API calls
 class APIManager{
 
     constructor(rankingAddress = '', indexingAddress = '') {
         this.rankingAddress = rankingAddress;
         this.indexingAddress = indexingAddress;
-        this.finishedAPI=false;
     }
 
     setRankingAddress(address) {
@@ -18,56 +19,79 @@ class APIManager{
         console.log(docID);
         const Http = new XMLHttpRequest();
         const url=this.indexingAddress + docID;
-        Http.open("GET", url);
-        this.finishedAPI=false;
-        this.callAPI(Http);
-        
-        var i=0;
-         while(!this.finishedAPI){
-            i++;
-            if(i==10000){
-                break;
-            }             
-         }
-         console.log("finished: "+this.finishedAPI);
-        // return x;
-    };
-
-    callAPI(Http){
+        //make API call synchronus to allow proper functionality
+        Http.open("GET", url, false);
         Http.send();
-        Http.onreadystatechange=(e)=>{
-            //console.log(Http.responseText);
-            let obj = JSON.parse(Http.responseText);
-            console.log(obj);
-            this.finishedAPI=true;
-            //updateUI(obj.pages, startResult, startResult+maxresults);
-
-        }
-    }
-
+        let obj =  JSON.parse(Http.responseText);
+        console.log(obj);
+        return obj.data;
+    };
     searchRequest(queryInfo, maxresults=10, startResult=0) {
         console.log("passing in query Info, maxresults, startresult")
         console.log(queryInfo)
         console.log(maxresults)
         console.log(startResult)
+
         const Http = new XMLHttpRequest();
         const url=this.rankingAddress + queryInfo;
-        Http.open("GET", url);
+        Http.open("GET", url, false);
         Http.send();
-        Http.onreadystatechange=(e)=>{
-            //console.log(Http.responseText);
-            let obj =  JSON.parse(Http.responseText);
-            console.log(obj);
-            updateUI(obj.docs, startResult, startResult+maxresults);
-
-        }
+        let obj =  JSON.parse(Http.responseText);
+        console.log(obj);
+        Manager.updateUI(obj.pages, startResult, startResult+maxresults);
     };
 
     sendQueryFeedback(docID) {
     };
 }
+
+
+//class to deal with updating UI
+class UIManager{
+    construct(){
+
+    };
+
+    updateUI(results, start, end){
+          console.log(results);  
+
+          var i;
+          //grab the element that we will put our result divs into
+          var container=document.getElementById("info");
+          container.innerHTML="";
+          //console.log(results[start]);
+
+          //console.log()
+            
+          for(i=start; i<end && i<results.length; i++){
+            //var snip=new Snippet("header", "google.com", "body", "snippet");
+            //add in code to use snippet generator
+            
+            var gen=new SnippetsGenerator();
+            var snip=gen.getSnippets(results[i].docid, results[i].keywords);
+
+            var div=document.createElement("div");
+            div.class="resultDiv";
+
+            //create the link for the html
+            var url=document.createElement("a");
+            url.href=snip.url;
+            url.innerText=snip.title;
+
+            //console.log(url);
+
+            //append everything to the div element
+            div.appendChild(url);
+            div.appendChild(document.createElement("br"));  //new line element
+            div.appendChild(document.createTextNode(snip.snippet)); //add in the text snippet
+            //append the div to the container element
+            container.appendChild(div);
+
+            console.log(div);
+          }
+    }
+}
+
+//create global variables
 let API = new APIManager("http://green-x.cs.rpi.edu:8080/search?query=", "http://green-eth.cs.rpi.edu/querying?id=");
-// Get search result for a given query as parameter
-//API.searchRequest('');
-// Get a document object (as described in deliverable 1)
-//API.getDocument(1);
+let Manager = new UIManager();
