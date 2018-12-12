9a2204fc34fea8011b75be0d99ad4db4e9c0e457
Jared <segalj@rpi.edu>
2018-12-12 11:57:38 -0500

    added checks to try and create examples even with ranking and indexing out of sync

--- UI/javascript/APIManager.js
+++ UI/javascript/APIManager.js
@@ -17,6 +17,7 @@ class APIManager{
 
     getDocument(docID) {
         console.log(docID);
+        //docID="fd34edfe67a924377d8a9dfe9a78a38a";       //This value is hard coded so that we may show entire functionality of UIcomponent
         const Http = new XMLHttpRequest();
         const url=this.indexingAddress + docID;
         //make API call synchronus to allow proper functionality
@@ -45,6 +46,13 @@ class APIManager{
     };
 }
 
+//this is the format of a class that should be passed by the API calls to ranking
+class json{
+    constructor(docid, keywords){
+        this.docid = docid;
+        this.keywords = keywords;
+    };
+}
 
 //class to deal with updating UI
 class UIManager{
@@ -53,12 +61,20 @@ class UIManager{
     };
 
     updateUI(results, start, end){
-          console.log(results);  
+          console.log(results);
+          if(results.length==0){    //added in for ranking not having any documents returning
+            results=[new json("0a137b375cc3881a70e186ce2172c8d1", ["google", "Google"]), new json("fd34edfe67a924377d8a9dfe9a78a38a", ["bing", "BING"])];
+          } 
+          console.log(results); 
 
+          
+          var gen=new SnippetsGenerator();
           var i;
-          //grab the element that we will put our result divs into
-          var container=document.getElementById("info");
-          container.innerHTML="";
+
+          //take the info node, remove everything in it, and copy it for future use
+          var resultTemplate=document.getElementById("info");
+          resultTemplate.innerHTML="";
+          var container=document.getElementById("content");
           //console.log(results[start]);
 
           //console.log()
@@ -67,15 +83,16 @@ class UIManager{
             //var snip=new Snippet("header", "google.com", "body", "snippet");
             //add in code to use snippet generator
             
-            var gen=new SnippetsGenerator();
+            
+            console.log(results[i]);
             var snip=gen.getSnippets(results[i].docid, results[i].keywords);
 
-            var div=document.createElement("div");
+            var div=resultTemplate.cloneNode(false);
             div.class="resultDiv";
 
             //create the link for the html
             var url=document.createElement("a");
-            url.href=snip.url;
+            url.href="https://"+snip.url;
             url.innerText=snip.title;
 
             //console.log(url);
@@ -85,6 +102,7 @@ class UIManager{
             div.appendChild(document.createElement("br"));  //new line element
             div.appendChild(document.createTextNode(snip.snippet)); //add in the text snippet
             //append the div to the container element
+            console.log("div", div);
             container.appendChild(div);
 
             console.log(div);
